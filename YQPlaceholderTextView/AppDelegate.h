f20b52f1aafa0794bab636916ecf6dbb8713d593
yuyedaidao <wyqpadding@gmail.com>
2015-12-06 16:51:00 +0800

    Initial Commit

---
+++ YQPlaceholderTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQPlaceholderTextView
+//
+//  Created by 王叶庆 on 15/12/6.
+//  Copyright © 2015年 王叶庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YQPlaceholderTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQPlaceholderTextView
+//
+//  Created by 王叶庆 on 15/12/6.
+//  Copyright © 2015年 王叶庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
